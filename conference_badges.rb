18a9b0ccee194424530cd9bab0ab2753cb7311ea
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-24 19:17:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,10 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  batch_badge_creator(array).each
-  assign_rooms(array).each
+  batch_badge_creator(array).each do |index|
+    puts index
+  end
+  assign_rooms(array).each do |index|
+    puts index
+  end
 end
