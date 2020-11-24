b049483ae95e02df7f6dbf5385b9c60649627229
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-24 19:00:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,13 +4,13 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-=begin
 def batch_badge_creator(array)
   array.each |index|
     badge_maker(index)
   end
 end
 
+=begin
 def assign_rooms(array)
   array.each_with_index |val, index|
     put "Hello, #{val}! You'll be assigned to room #{index + 1}!"
